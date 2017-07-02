38ca98f84b8d0c9b64bd9d66f5bd615b12dd86f8
Renato <renatu.anjos@gmail.com>
2017-07-02 11:45:26 -0300

    Exercicio - Tabuada de 1 à 10

--- exercicios_python.py
+++ exercicios_python.py
@@ -38,8 +38,8 @@ x = 1
 fat = int(input("Digite um numero"))
 while i <= fat:
     x = x * i
-    i = i+ 1
-print("Fator(%d) = %d"%(fat, x)) 
+    i = i + 1
+print("Fator(%d) = %d" %(fat, x)) 
 
 '''
 	Tabuada
@@ -48,7 +48,7 @@ tabuada = 1
 while tabuada <= 10:
     n = 1
     print("Tabuada %d" %tabuada)
-    while n <=10:
+    while n <= 10:
         print("%d x %d = %d" %(tabuada, n, tabuada * n))
         n = n + 1
-    tabuada = tabuada +1
+    tabuada = tabuada + 1
